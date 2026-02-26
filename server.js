0d17d33e44164a440edc2b343790e32aa2b44640
ritwikfilms-sketch <ritwikfilms@gmail.com>
2026-02-26 20:15:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ritwik AI Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(⁠ Server running on port ${PORT} ⁠);
+});
+
 
